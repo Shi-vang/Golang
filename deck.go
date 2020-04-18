8543f6ef9c7532756745bc67453f04be459db032
shivanggarg <shivanggarg48@gmail.com>
2020-04-18 15:39:26 +0530

    add deal function

--- deck.go
+++ deck.go
@@ -24,3 +24,7 @@ func newDeck() deck {
 	}
 	return card
 }
+
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
--- main.go
+++ main.go
@@ -6,7 +6,11 @@ func main() {
 	//var card string = "Ace of spade"
 	splic1 := newDeck()
 	splic2 := newDeck()
+	hand1, remainhand1 := deal(splic1, 3)
 	splic1.print()
+	fmt.Println(hand1, remainhand1)
 	fmt.Println("  ")
+	hand2, remainhand2 := deal(splic2, 7)
+	fmt.Println(hand2, remainhand2)
 	splic2.print()
 }
